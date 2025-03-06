5c840cac59e22ae6df9d1b8fc332562e89a474fb
MARG210105 <xgaleanamoreno@gmail.com>
2025-03-06 14:45:54 -0600

    Create Tarea-Ejercicio3.c

---
+++ Tarea-Ejercicio3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int res = 0;
+int i;
+
+for(i = 0; i < 11; i++)
+{
+res = 0;
+}
+printf("Solucion 1 - El numero es: %d\n" , res);
+for(i = 0; i <= 10; i++)
+{
+   res = res + i;
+}
+printf("Solucion 2 - El numero es: %d\n" , res);
+for(i = 0; i < 11; i++)
+{
+res = 0;
+}
+printf("Solucion 1 - El numero es: %d\n" , res);
+for(i = 0; i < 11; i++)
+{
+res = 0;
+}
+printf("Solucion 1 - El numero es: %d\n" , res);
